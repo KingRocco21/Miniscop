00513133e746f19ca4079c9e28c3d2c8cf095b73
Anthony Pagone <anthony21@pagone.com>
2025-06-13 14:30:49 -0700

    Got the title rotation working!

--- src/plugins/overworld.rs
+++ src/plugins/overworld.rs
@@ -1,23 +1,27 @@
 use crate::states::AppState;
+use bevy::math::ops::{cos, sin};
 use bevy::prelude::*;
+use std::f32::consts::PI;
 
 pub struct OverworldPlugin;
 impl Plugin for OverworldPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(AppState::Overworld), setup_overworld);
+        app.add_systems(OnEnter(AppState::Overworld), setup_overworld)
+            .add_systems(Update, update_title.run_if(in_state(AppState::Overworld)));
     }
 }
 
-fn setup_overworld(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
+// Components
+#[derive(Component)]
+struct Rotatable;
+
+// Systems
+fn setup_overworld(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Main 3D camera
     commands.spawn((
         StateScoped(AppState::Overworld),
         Camera3d::default(),
-        Transform::from_xyz(0.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(0.0, 0.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
     commands.spawn((
         StateScoped(AppState::Overworld),
@@ -29,8 +33,14 @@ fn setup_overworld(
     commands.spawn((
         StateScoped(AppState::Overworld),
         SceneRoot(asset_server.load(GltfAssetLabel::Scene(0).from_asset("mainmenu/3d/Title.glb"))),
+        Transform::default(),
+        Rotatable,
     ));
-    // Animate with these functions:
-    // sin(2*pi*x)*cos(pi/6*x)
-    // sin(2*pi*x)*sin(pi/6*x)
+}
+
+fn update_title(mut title: Single<&mut Transform, With<Rotatable>>, mut time: Res<Time>) {
+    let seconds = time.elapsed_secs();
+    let theta_y = 15.0 * sin(2.0 * PI * seconds) * cos(PI / 6.0 * seconds) * PI / 180.0;
+    let theta_z = 15.0 * sin(2.0 * PI * seconds) * sin(PI / 6.0 * seconds) * PI / 180.0;
+    title.rotation = Quat::from_euler(EulerRot::XYZEx, 0.0, theta_y, theta_z);
 }
